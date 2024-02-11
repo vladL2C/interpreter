077e40a82398c03f86dd54ed11a01f67a414ac73
vlad <vlad@easycrypto.ai>
2024-02-11 14:51:39 +1300

    feat: add string literals to interpreter

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -84,7 +84,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return args[0]
 		}
 		return applyFunction(function, args)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
+
 	return nil
 }
 
@@ -226,9 +229,20 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -25,6 +25,8 @@ func TestNextToken(t *testing.T) {
 
 		10 == 10; 
 		10 != 9;
+		"foobar";
+		"foo bar";
 	   `
 
 	tests := []struct {
@@ -104,6 +106,11 @@ func TestNextToken(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.SEMICOLON, ";"},
+		{token.STRING, "foo bar"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
