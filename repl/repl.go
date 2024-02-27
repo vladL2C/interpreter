8ffd469f62d0f4ead58a4f1736da4b100ac36928
vlad <vlad@easycrypto.ai>
2024-02-27 14:06:26 +1300

    adds built in len func

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,6 @@ package repl
 
 import (
 	"bufio"
-	"fmt"
 
 	"io"
 
@@ -20,7 +19,6 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
 	for {
-		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
