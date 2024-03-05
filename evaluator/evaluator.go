22118c26d9034c3068843af9a2dad99a69391203
vlad <vlad@easycrypto.ai>
2024-03-05 15:52:35 +1300

    remove unsused

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -290,17 +290,6 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 }
 
-func evalStatements(stmts []ast.Statement, env *object.Environment) object.Object {
-	var result object.Object
-	for _, statement := range stmts {
-		result = Eval(statement, env)
-		if returnValue, ok := result.(*object.ReturnValue); ok {
-			return returnValue.Value
-		}
-	}
-	return result
-}
-
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
