7dcc6c7cc44f45adf8c4497706137148a0229f9b
Vlad <v.saraev@cultureamp.com>
2024-04-11 12:19:08 +1200

    add simple bytecode compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,43 @@
+package compiler
+
+import (
+	"github.com/vladl2c/interpreter/ast"
+	"github.com/vladl2c/interpreter/code"
+	"github.com/vladl2c/interpreter/lexer"
+	"github.com/vladl2c/interpreter/object"
+	"github.com/vladl2c/interpreter/parser"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
