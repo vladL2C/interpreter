a0148833620cbdacd9ba7d88360dbffcfee328ee
Vlad <v.saraev@cultureamp.com>
2024-04-15 15:37:33 +1200

    add compiler repl

--- repl/repl.go
+++ repl/repl.go
@@ -2,20 +2,23 @@ package repl
 
 import (
 	"bufio"
+	"fmt"
 
 	"io"
 
+	"github.com/vladl2c/interpreter/compiler"
 	"github.com/vladl2c/interpreter/evaluator"
 	"github.com/vladl2c/interpreter/lexer"
 	"github.com/vladl2c/interpreter/object"
 	"github.com/vladl2c/interpreter/parser"
+	"github.com/vladl2c/interpreter/vm"
 )
 
 const PROMPT = ">> foo = 5;"
 
 const MONKEY_FACE = `MONKEY DOOOOO`
 
-func Start(in io.Reader, out io.Writer) {
+func StartInterpreter(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
 	for {
@@ -48,5 +51,44 @@ func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+func StartCompiler(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
